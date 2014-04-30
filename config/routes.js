886629d217e558d3466deef3ef5a6542be7e7ef4
Aviv Rozenboim <rubysrubys@gmail.com>
2014-04-30 17:08:48 +0300

    Fixed something lost in merging

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,7 @@
 var express = require('express'),
     path = require('path'),
     tagController = require('../app/controllers/tag.js'),
-    applicationController = require('../app/controllers/application.js'),
-    usersController = require('../app/controllers/users.js');
+    applicationController = require('../app/controllers/application.js');
 
 module.exports = function(app) {
   var authController = require('../app/controllers/auth.js')(app);
