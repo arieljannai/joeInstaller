8393527c51760332034cf6420d947907b1b6abb5
Aviv Rozenboim <rubysrubys@gmail.com>
2014-04-30 19:28:50 +0300

    Fixed applications appearing in tags when i dont want them to!

--- app/controllers/tag.js
+++ app/controllers/tag.js
@@ -2,11 +2,8 @@ var Tag = require('../models/tag');
 
 // GET all tags, without their relevant applications
 exports.getTags = function (req, res) {
-    Tag.find(function (err, tags) {
-        res.json(tags.map(function (tag) {
-            delete tag['applications'];
-            return tag;
-        }));
+    Tag.find({}, "-applications", function (err, tags) {
+        res.json(tags);
     });
 };
 
@@ -24,7 +21,7 @@ exports.getTag = function (req, res) {
 
 // GET the fivemost popular tags, sorted by applications_count
 exports.getPopularTags = function (req, res) {
-    Tag.find().sort({applications_count : -1}).limit(5).exec(
+    Tag.find({}, "-applications").sort({applications_count : -1}).limit(5).exec(
         function (err, tags) {
             if (err)
                 return console.error(err);
