4b424de1d2cddfa5b5d6d538c6e3cfc03d58287b
Ariel Jannai <arieljannai@gmail.com>
2014-05-01 08:58:46 +0300

    added controllers for tags and users

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,22 @@ joeApp.controller('AppsCtrl', ['$scope', 'Application', function ($scope, Applic
     $scope.applications = Application.query();
 }]);
 
+joeApp.controller('TagsCtrl', ['$scope', 'Tags', function ($scope, Tags) {
+    $scope.tags = Tags.query();
+}]);
+
+joeApp.controller('UsersCtrl', ['$scope', 'Users', function ($scope, Users) {
+    $scope.users = Users.query();
+}]);
 
 joeApp.factory('Application', ['$resource', function ($resource) {
     return $resource('/applications');
+}]);
+
+joeApp.factory('Tag', ['$resource', function ($resource) {
+    return $resource('/tags');
+}]);
+
+joeApp.factory('User', ['$resource', function ($resource) {
+    return $resource('/users');
 }]);
